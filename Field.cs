c8665600c09cd86ca2bedb842bfd10c9680dd683
Данил Тимофеев <danil2006timofeev@gmail.com>
2023-12-14 09:51:47 +0300

    ssb

--- Field.cs
+++ Field.cs
@@ -10,23 +10,23 @@ internal class Field
     }// тут организовать синглтон и хранение ячеек в свойстве Cells
 
     public int[,] Cells { get; set; }
-    public int[] finishCoordinates { get; set; }
 
     internal bool CheckRobotEndGame(Robot robot)
     {
         int[] fc = new int[2];
+
         for (int i = 0; i < 5; i++)
             for (int j = 0; j < 5; j++)
                 if (Cells[i, j] == 4)
                 {
                     fc[0] = i;
                     fc[1] = j;
-                    finishCoordinates = fc;
-                    Console.WriteLine(robot.robotCoordinates[0] + " " + robot.robotCoordinates[1]);
-                    Console.WriteLine(finishCoordinates[0] + " " + finishCoordinates[1]);
+
+                    //Console.WriteLine(robot.robotCoordinates[0] + " " + robot.robotCoordinates[1]);
+                    //Console.WriteLine(fc[0] + " " + fc[1]);
                     continue;
                 }
-        if (robot.robotCoordinates == finishCoordinates)
+        if (robot.robotCoordinates == fc)
             return false;
         else return true;//сравнение координат робота с координатамифинишной точки
         
